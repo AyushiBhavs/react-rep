b414c4efccac3f25c242b3e4c3557f1afb833aff
Ayushi Bhavsar <bhavsar.ayu17@gmail.com>
2021-11-17 16:19:45 +0530

    create loggedIn state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/Login';
 import DashBoard from './pages/DashBoard';
 
- function App() {
+
+const dashContext = React.createContext()
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <>
-     {/* <DashBoard/> */}
-     <Login/>
+      <dashContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+        {isLoggedIn ? <DashBoard /> : <Login />}
+      </dashContext.Provider>
+
     </>
   )
 }
 
 export default App;
+export { dashContext }
